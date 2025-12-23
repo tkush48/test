a994c20ce0b1c7d8a2655e30cffc29290c1c20d6
tkush48 <kushagra4288@gmail.com>
2025-12-23 21:44:20 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -10,11 +10,21 @@ data "aws_iam_policy_document" "assume_role" {
     actions = ["sts:AssumeRole"]
   }
 }
-
+data "aws_iam_policy_document" "lambda_ecr" {
+	statement {
+		effect = "Allow"
+        actions = [
+			"ecr:*"
+		]
+		resources = [
+			for repo in aws_ecr_repository.ecr : repo.arn
+		]
+}
 resource "aws_iam_role" "lambda_role" {
   name = "lambda-role-${var.environment}"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
+
 
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_role.name
@@ -25,6 +35,14 @@ resource "aws_iam_role_policy_attachment" "lambda_vpc" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
+resource "aws_iam_policy" "lambda_ecr_policy" {
+	name = "lambda-ecr-policy-${var.environment}"
+	policy = data.aws_iam_policy_document.lambda_ecr.json
+}
+resource "aws_iam_role_policy_attachment" "lambda_ecr_pa" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_ecr_policy
+}
 
 resource "aws_lambda_function" "lambda_function" {
   for_each = var.lambda_functions
@@ -32,7 +50,8 @@ resource "aws_lambda_function" "lambda_function" {
   function_name = "${each.key}-${var.environment}"
   role          = aws_iam_role.lambda_role.arn
   package_type  = "Image"
-  image_uri     = "${aws_ecr_repository.ecr[each.value.ecr_repository].repository_url}:LATEST"
+  image_uri     = "public.ecr.aws/lambda/nodejs:18" #dummy image for initialization
+  #image_uri     = "${aws_ecr_repository.ecr[each.value.ecr_repository].repository_url}:LATEST"
   timeout       = each.value.timeout
   memory_size   = each.value.memory_size
   vpc_config {
